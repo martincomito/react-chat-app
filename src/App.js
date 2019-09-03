f72129dfa89d126db83a7bc17877dc41c53bccd5
Martin Comito <martin.comito@globallogic.com>
2019-09-03 16:39:03 -0300

    Added Header, Container and LiveVisitors components to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Container, Row } from 'reactstrap';
+import Header from './components/Header.component';
+import LiveVisitors from './components/LiveVisitors.component';
 
-function App() {
-  return (
-    <div className='App'>
-      <p>Testing</p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Container>
+          <Row>
+            <LiveVisitors />
+          </Row>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
