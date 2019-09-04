f32b3b39fc3f8ca8e26313c74d9a038994f9da16
Martin Comito <martin.comito@globallogic.com>
2019-09-04 12:17:46 -0300

    Code refactor to use only the layout component in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Container, Row } from 'reactstrap';
-import Header from './components/Header.component';
-import LiveVisitors from './components/LiveVisitors.component';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from './containers/layout';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Header />
-        <Container>
-          <Row>
-            <LiveVisitors />
-          </Row>
-        </Container>
+        <BrowserRouter>
+          <Layout />
+        </BrowserRouter>
       </React.Fragment>
     );
   }
