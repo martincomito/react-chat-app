28dc84a328b852261296eb12a7725a009759a4ca
Martin Comito <martin.comito@globallogic.com>
2019-09-04 12:17:06 -0300

    Added nav links to navbar

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav } from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
+import { Link } from 'react-router-dom';
 class Header extends React.Component {
   state = {
     isOpen: false,
@@ -16,10 +25,23 @@ class Header extends React.Component {
     return (
       <div>
         <Navbar color='dark' dark expand='md'>
-          <NavbarBrand href='/'>React chat app</NavbarBrand>
+          <NavbarBrand tag={Link} to='/'>
+            React chat app
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className='ml-auto' navbar></Nav>
+            <Nav className='ml-2' navbar>
+              <NavItem>
+                <NavLink tag={Link} to='/chatroom'>
+                  Chat room
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to='/livevisitors'>
+                  Live visitors
+                </NavLink>
+              </NavItem>
+            </Nav>
           </Collapse>
         </Navbar>
       </div>
