067f3e22708d923bcb607d1c29c3c90a005d18f0
Martin Comito <martin.comito@globallogic.com>
2019-09-09 14:02:51 -0300

    Added chatRoom and selectnameInput to PublicCjhat component

--- src/components/PublicChat.component.jsx
+++ src/components/PublicChat.component.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import SelectNameInput from './SelectNameInput.component';
+import ChatRoom from './ChatRoom.component';
 
-const PublicChat = () => <p>PublicChat</p>;
+class PublicChat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      isLogged: false,
+      isDisabled: true,
+    };
+  }
+
+  handleNameInputChange = event => {
+    this.setState({ inputValue: event.target.value }, () =>
+      console.log(this.state.inputValue)
+    );
+  };
+
+  handleClickJoin = () => {
+    if (this.state.isDisabled) {
+      this.setState({ isLogged: true }, () => console.log(this.state.isLogged));
+    }
+  };
+
+  render() {
+    return this.state.isLogged ? (
+      <ChatRoom userLogged={this.state.inputValue} />
+    ) : (
+      <SelectNameInput
+        handleNameInputChange={this.handleNameInputChange}
+        handleClickJoin={this.handleClickJoin}
+        inputValue={this.state.inputValue}
+      />
+    );
+  }
+}
 
 export default PublicChat;
