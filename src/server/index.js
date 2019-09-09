732f1eca08300e73c6aac4f3b8d28ab4128b1881
Martin Comito <martin.comito@globallogic.com>
2019-09-09 14:00:47 -0300

    changed ports

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-const port = 6600;
+const port = 6700;
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
