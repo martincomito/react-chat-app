259dd6af0723cc8b72e63836bb9284df35a87219
Martin Comito <martin.comito@globallogic.com>
2019-09-09 13:59:36 -0300

    Created chatroom component

--- src/components/ChatRoom.component.jsx
+++ src/components/ChatRoom.component.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import RoomList from './RoomList.component';
+import ConnectedUsers from './ConnectedUsers.component';
 
-const ChatRoom = () => <p>ChatRoom!</p>;
+class ChatRoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      connectedUsers: [
+        { name: 'Juan', id: '1' },
+        { name: 'Pedro', id: '2' },
+        { name: 'Lucas', id: '3' },
+        { name: 'Paola', id: '4' },
+        { name: 'Carlos', id: '5' },
+        { name: 'Mauro', id: '6' },
+        { name: 'Tomás', id: '7' },
+        { name: 'Juana', id: '8' },
+        { name: 'Cecilia', id: '9' },
+        { name: 'Mariano', id: '10' },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div style={{ width: '70%' }}>
+          <RoomList loggedUserName={this.props.userLogged} />
+        </div>
+        <div style={{ width: '30%' }}>
+          <ConnectedUsers connectedUsers={this.state.connectedUsers} />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default ChatRoom;
